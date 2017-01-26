cacac6016ca946b04a76c75d4ba89c89f6b536a4
Rafael Miranda <rafacarmir@gmail.com>
2017-01-26 11:05:59 +0100

    Added the Interface APIKeys.java to the repo
    You will still need to change the value of the Key for it to work.

---
+++ app/src/main/java/de/alfingo/whattowatch/Utilities/APIKeys.java
@@ -0,0 +1,10 @@
+package de.alfingo.whattowatch.Utilities;
+
+/**
+ * @author Rafael
+ * @version 0.1
+ * @since 25.01.2017
+ */
+public interface APIKeys {
+    String MOVIEDB_API_KEY = "COPY_YOUR_OWN_API_KEY_HERE";
+}
